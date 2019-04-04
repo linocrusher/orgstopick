4bfec38c47cd74b2fda60d0228eed221968346c7
Eizer G. Relayson <egrelayson@up.edu.ph>
2019-04-04 22:23:24 +0800

    Finished search function.

--- app/controllers/orgs_controller.rb
+++ app/controllers/orgs_controller.rb
@@ -1,6 +1,11 @@
 class OrgsController < ApplicationController
   def index
     @orgs = Org.all
+    if params[:noa] == "1"
+      @orgs = Org.where("name like ?", "%" + params[:keyword] + "%")
+    elsif params[:noa] == "2"
+      @orgs = Org.where("acronym like ?", "%" + params[:keyword] + "%")
+    end
   end
 
   def show
@@ -20,6 +25,6 @@ class OrgsController < ApplicationController
 
   private
   def org_params
-    params.require(:org).permit(:name, :acronym, :college, :department, :scope, :description)
+    params.require(:org).permit(:name, :acronym, :college, :department, :scope, :description, :noa, :keyword)
   end
 end
